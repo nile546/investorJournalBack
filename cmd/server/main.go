abbd218e9e026b2ba9fb45f12d4e1920921bb4f0
Denis Anisimov <vysheradugi4@gmail.com>
2020-12-26 14:59:17 -0500

    Added loading config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+// Config ...
+type Config struct {
+	Address string
+}
+
+func getConf() {
+	c := "./config/config.toml"
+	var conf Config
+
+	d, err := toml.DecodeFile(c, &conf)
+	if err != nil {
+		fmt.Println("Cant loading config", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(d)
+}
 
 func main() {
-	fmt.Println("Hello world!")
-	fmt.Println("Hello world!")
+	getConf()
 }
