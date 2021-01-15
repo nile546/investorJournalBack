3235813b574c6cdfdeef388b37a147d80f0124f4
Nikita <nikez@inbox.ru>
2021-01-15 23:56:49 +0300

    add index api

--- internal/apiserver/apiserver.go
+++ internal/apiserver/apiserver.go
@@ -3,6 +3,8 @@ package apiserver
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 	"github.com/nile546/diplom/config"
@@ -17,19 +19,45 @@ type server struct {
 	router *mux.Router
 }
 
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.router.ServeHTTP(w, r)
 }
 
+func (s spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path, err := filepath.Abs(r.URL.Path)
+	fmt.Println("zzzz", r.URL.Path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	path = filepath.Join(s.staticPath, path)
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		http.ServeFile(w, r, filepath.Join(s.staticPath, s.indexPath))
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.FileServer(http.Dir(s.staticPath)).ServeHTTP(w, r)
+}
+
 func (s *server) ConfugureRouter() {
 
-	s.router.HandleFunc("/test", test)
+	//s.router.HandleFunc("/test", test)
+	spa := spaHandler{staticPath: "web", indexPath: "index.html"}
 
+	s.router.PathPrefix("/").Handler(spa)
 }
 
-func test(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("111")
-}
+//func test(w http.ResponseWriter, r *http.Request) {
+//	fmt.Println("111")
+//}
 
 //Start ...
 func Start(c *config.Config) error {
