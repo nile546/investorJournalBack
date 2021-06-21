fd1d2eb5e0f114e5ac90adb5840cb94fe240fce6
Nikita <nikez@inbox.ru>
2021-06-21 15:16:34 +0300

    update strategy name to *

--- internal/models/strategy.go
+++ internal/models/strategy.go
@@ -4,7 +4,7 @@ import "time"
 
 type Strategy struct {
 	ID             *int64          `json:"id"`
-	Name           string          `json:"name"`
+	Name           *string         `json:"name"`
 	Description    *string         `json:"description"`
 	UserID         *int64          `json:"userId"`
 	InstrumentType InstrumentTypes `json:"instrumentType"`
