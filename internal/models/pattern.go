ca67c32b14462d0d731af69f7ee614a5c7fa3193
Denis Anisimov <vysheradugi4@gmail.com>
2021-06-21 13:12:14 -0400

    Added build, fixed nil issues

--- internal/models/pattern.go
+++ internal/models/pattern.go
@@ -3,11 +3,19 @@ package models
 import "time"
 
 type Pattern struct {
-	ID             *int64          `json:"id"`
+	ID             int64           `json:"id"`
 	Name           *string         `json:"name"`
 	Description    *string         `json:"description"`
 	Icon           *string         `json:"icon"`
 	UserID         *int64          `json:"userId"`
 	InstrumentType InstrumentTypes `json:"instrumentType"`
 	CreatedAt      time.Time       `json:"createdAt"`
+}
+
+func (p *Pattern) GetID() *int64 {
+	if p == nil {
+		return nil
+	}
+
+	return &p.ID
 }
--- internal/models/strategy.go
+++ internal/models/strategy.go
@@ -3,10 +3,18 @@ package models
 import "time"
 
 type Strategy struct {
-	ID             *int64          `json:"id"`
+	ID             int64           `json:"id"`
 	Name           *string         `json:"name"`
 	Description    *string         `json:"description"`
-	UserID         *int64          `json:"userId"`
+	UserID         *int64          `json:"userId,omitempty"`
 	InstrumentType InstrumentTypes `json:"instrumentType"`
 	CreatedAt      time.Time       `json:"createdAt"`
+}
+
+func (s *Strategy) GetID() *int64 {
+	if s == nil {
+		return nil
+	}
+
+	return &s.ID
 }
