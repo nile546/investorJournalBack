5ba142a39e62533cb617107cec99c60f355f51f6
Nikita <nikez@inbox.ru>
2021-06-21 15:04:27 +0300

    update strategy, pattern

--- internal/models/pattern.go
+++ internal/models/pattern.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type Pattern struct {
-	ID             int64           `json:"id"`
+	ID             *int64          `json:"id"`
 	Name           string          `json:"name"`
 	Description    *string         `json:"description"`
 	Icon           *string         `json:"icon"`
--- internal/models/strategy.go
+++ internal/models/strategy.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type Strategy struct {
-	ID             int64           `json:"id"`
+	ID             *int64          `json:"id"`
 	Name           string          `json:"name"`
 	Description    *string         `json:"description"`
 	UserID         *int64          `json:"userId"`
